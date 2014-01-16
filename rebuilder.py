19d69411798b3aac718ba7d5f4b4cd0a60d834e1
spl0k <alban.feron@gmail.com>
2014-01-16 22:39:39 +0100

    Adding channel.lastBuildDate and channel.generator

--- rebuilder.py
+++ rebuilder.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-import sys
+import sys, time
 import feedparser
 from xml.etree import ElementTree
 
@@ -24,9 +24,8 @@ channel_optional = [
 ] # lastBuildDate, generator, cloud, image, textInput
 item_required = [
 	'title',
-	'link',
-	'description'
-]
+	'link'
+] # description
 item_optional = [
 	'author',
 	'category',
@@ -35,12 +34,7 @@ item_optional = [
 	('published', 'pubDate')
 ] # enclosure, source
 
-source = feedparser.parse(sys.argv[1])
-
-root = ElementTree.Element('rss')
-root.set('version', '2.0')
-
-def put_elems(source, elems, xml_elem, required = False):
+def putback_elems(source, elems, xml_elem, required = False):
 	for elem in elems:
 		if isinstance(elem, tuple):
 			attr = elem[0]
@@ -52,14 +46,25 @@ def put_elems(source, elems, xml_elem, required = False):
 		if required or hasattr(source, attr):
 			ElementTree.SubElement(xml_elem, tag).text = getattr(source, attr)
 
+source = feedparser.parse(sys.argv[1])
+
+root = ElementTree.Element('rss')
+root.set('version', '2.0')
+
 channel = ElementTree.SubElement(root, 'channel')
-put_elems(source.feed, channel_required, channel, True)
-put_elems(source.feed, channel_optional, channel)
+putback_elems(source.feed, channel_required, channel, True)
+putback_elems(source.feed, channel_optional, channel)
+
+ElementTree.SubElement(channel, 'lastBuildDate').text = time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime())
+if hasattr(source.feed, 'generator'):
+	ElementTree.SubElement(channel, 'generator').text = source.feed.generator + ' & RSS Rebuilder'
+else:
+	ElementTree.SubElement(channel, 'generator').text = 'RSS Rebuilder'
 
 for entry in source.entries:
 	item = ElementTree.SubElement(channel, 'item')
-	put_elems(entry, item_required, item, True)
-	put_elems(entry, item_optional, item)
+	putback_elems(entry, item_required, item, True)
+	putback_elems(entry, item_optional, item)
 
 ElementTree.ElementTree(root).write(sys.argv[2], 'utf-8')
 
