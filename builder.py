e91c857259983f30d419fc837177c2361071a46c
spl0k <alban.feron@gmail.com>
2019-05-29 22:42:38 +0200

    Untabbify

--- builder.py
+++ builder.py
@@ -3,87 +3,87 @@
 import time, argparse, sys
 
 def get_cmdline_args():
-	argparser = argparse.ArgumentParser()
-	argparser.add_argument('url', help = 'URL to get the list of articles from')
-	argparser.add_argument('list_selector', help = 'CSS selector to retreive items URLs (<a> tags)')
-	argparser.add_argument('item_selector', help = 'CSS selector used to extract the relevant content from the URL the previous selector returned.')
-	argparser.add_argument('output', help = 'Path of the resulting RSS file. Use "-" for stdout')
-	argparser.add_argument('-p', '--pretty', action = 'store_true', help = 'Specify that the output should be prettyfied')
-	argparser.add_argument('--ignored-query-params', nargs = '*', default = [], help = 'Query parameters to remove when generating the links')
-	return argparser.parse_args()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('url', help = 'URL to get the list of articles from')
+    argparser.add_argument('list_selector', help = 'CSS selector to retreive items URLs (<a> tags)')
+    argparser.add_argument('item_selector', help = 'CSS selector used to extract the relevant content from the URL the previous selector returned.')
+    argparser.add_argument('output', help = 'Path of the resulting RSS file. Use "-" for stdout')
+    argparser.add_argument('-p', '--pretty', action = 'store_true', help = 'Specify that the output should be prettyfied')
+    argparser.add_argument('--ignored-query-params', nargs = '*', default = [], help = 'Query parameters to remove when generating the links')
+    return argparser.parse_args()
 
 def new_tag(tag, string):
-	t = Tag(name = tag)
-	t.string = string
-	return t
+    t = Tag(name = tag)
+    t.string = string
+    return t
 
 def build_rss(url, list_selector, item_selector, ignored_qp, output, pretty = False):
-	try:
-		soup = BeautifulSoup('<rss version="2.0" />', 'xml')
-		rss = soup.rss
-		has_lxml = True
-	except FeatureNotFound:
-		rss = BeautifulSoup('<rss version="2.0" />').rss
-		has_lxml = False
+    try:
+        soup = BeautifulSoup('<rss version="2.0" />', 'xml')
+        rss = soup.rss
+        has_lxml = True
+    except FeatureNotFound:
+        rss = BeautifulSoup('<rss version="2.0" />').rss
+        has_lxml = False
 
-	r = requests.get(url)
-	list_html = (BeautifulSoup(r.text, 'lxml') if has_lxml else BeautifulSoup(r.text)).html
+    r = requests.get(url)
+    list_html = (BeautifulSoup(r.text, 'lxml') if has_lxml else BeautifulSoup(r.text)).html
 
-	channel = Tag(name = 'channel')
-	rss.append(channel)
-	channel.append(new_tag('title', list_html.head.title.string))
-	channel.append(new_tag('link', url))
-	channel.append(new_tag('description', '--'))
-	channel.append(new_tag('lastBuildDate', time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime())))
-	channel.append(new_tag('generator', 'RSS Builder'))
+    channel = Tag(name = 'channel')
+    rss.append(channel)
+    channel.append(new_tag('title', list_html.head.title.string))
+    channel.append(new_tag('link', url))
+    channel.append(new_tag('description', '--'))
+    channel.append(new_tag('lastBuildDate', time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime())))
+    channel.append(new_tag('generator', 'RSS Builder'))
 
-	item_urls = list_html.select(list_selector)
-	for item_url in map(lambda i: i['href'], item_urls):
-		item_url = urlparse.urljoin(url, item_url)
-		parsed = urlparse.urlparse(item_url)
-		query_params = urlparse.parse_qsl(parsed.query)
-		item_url = urlparse.urlunparse((
-			parsed.scheme,
-			parsed.netloc,
-			parsed.path,
-			parsed.params,
-			'&'.join([ k+'='+v for k, v in query_params if k not in ignored_qp ]),
-			parsed.fragment))
+    item_urls = list_html.select(list_selector)
+    for item_url in map(lambda i: i['href'], item_urls):
+        item_url = urlparse.urljoin(url, item_url)
+        parsed = urlparse.urlparse(item_url)
+        query_params = urlparse.parse_qsl(parsed.query)
+        item_url = urlparse.urlunparse((
+            parsed.scheme,
+            parsed.netloc,
+            parsed.path,
+            parsed.params,
+            '&'.join([ k+'='+v for k, v in query_params if k not in ignored_qp ]),
+            parsed.fragment))
 
-		r = requests.get(item_url)
-		item_html = (BeautifulSoup(r.text, 'lxml') if has_lxml else BeautifulSoup(r.text)).html
+        r = requests.get(item_url)
+        item_html = (BeautifulSoup(r.text, 'lxml') if has_lxml else BeautifulSoup(r.text)).html
 
-		item = Tag(name = 'item')
-		item.append(new_tag('title', item_html.head.title.string))
-		item.append(new_tag('link', item_url))
-		item.append(new_tag('description', str(item_html.select(item_selector)[0])))
-		channel.append(item)
+        item = Tag(name = 'item')
+        item.append(new_tag('title', item_html.head.title.string))
+        item.append(new_tag('link', item_url))
+        item.append(new_tag('description', str(item_html.select(item_selector)[0])))
+        channel.append(item)
 
-	out_func = lambda x: (x.prettify() if pretty else unicode(x)).encode('utf-8')
-	if output == '-':
-		out_file = sys.stdout
-		close_file = lambda: None
-	else:
-		out_file = open(output, 'w')
-		close_file = out_file.close
+    out_func = lambda x: (x.prettify() if pretty else unicode(x)).encode('utf-8')
+    if output == '-':
+        out_file = sys.stdout
+        close_file = lambda: None
+    else:
+        out_file = open(output, 'w')
+        close_file = out_file.close
 
-	if has_lxml:
-		out_file.write(out_func(soup))
-	else:
-		out_file.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
-		out_file.write(out_func(rss))
-	out_file.write('\n')
-	close_file()
+    if has_lxml:
+        out_file.write(out_func(soup))
+    else:
+        out_file.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
+        out_file.write(out_func(rss))
+    out_file.write('\n')
+    close_file()
 
 if __name__ == '__main__':
-	args = get_cmdline_args()
+    args = get_cmdline_args()
 
-	import feedparser, requests, urlparse
-	from bs4 import BeautifulSoup, Tag
-	try:
-		from bs4 import FeatureNotFound
-	except ImportError:
-		FeatureNotFound = ValueError
+    import feedparser, requests, urlparse
+    from bs4 import BeautifulSoup, Tag
+    try:
+        from bs4 import FeatureNotFound
+    except ImportError:
+        FeatureNotFound = ValueError
 
-	build_rss(args.url, args.list_selector, args.item_selector, args.ignored_query_params, args.output, args.pretty)
+    build_rss(args.url, args.list_selector, args.item_selector, args.ignored_query_params, args.output, args.pretty)
 
--- rebuilder.py
+++ rebuilder.py
@@ -4,145 +4,145 @@ import time, re, argparse, sys
 import feedparser, requests
 from bs4 import BeautifulSoup, Tag
 try:
-	from bs4 import FeatureNotFound
+    from bs4 import FeatureNotFound
 except ImportError:
-	FeatureNotFound = ValueError
+    FeatureNotFound = ValueError
 
 channel_required = [
-	'title',
-	'link',
-	'description'
+    'title',
+    'link',
+    'description'
 ]
 channel_optional = [
-	'language',
-	('rights', 'copyright'),
-	('author', 'managingEditor'),
-	('publisher', 'webMaster'),
-	('published', 'pubDate'),
-	'category',
-	'docs',
-	'ttl',
-	'rating',
-	'skipHours',
-	'skipDays'
+    'language',
+    ('rights', 'copyright'),
+    ('author', 'managingEditor'),
+    ('publisher', 'webMaster'),
+    ('published', 'pubDate'),
+    'category',
+    'docs',
+    'ttl',
+    'rating',
+    'skipHours',
+    'skipDays'
 ] # lastBuildDate, generator, cloud, image, textInput
 item_required = [
-	'title',
-	'link'
+    'title',
+    'link'
 ] # description
 item_optional = [
-	'author',
-	'category',
-	'comments',
-	('id', 'guid'),
-	('published', 'pubDate')
+    'author',
+    'category',
+    'comments',
+    ('id', 'guid'),
+    ('published', 'pubDate')
 ] # enclosure, source
 
 def putback_elems(source, elems, xml_elem):
-	for elem in elems:
-		if isinstance(elem, tuple):
-			attr = elem[0]
-			tag = elem[1]
-		else:
-			attr = elem
-			tag = elem
+    for elem in elems:
+        if isinstance(elem, tuple):
+            attr = elem[0]
+            tag = elem[1]
+        else:
+            attr = elem
+            tag = elem
 
-		if hasattr(source, attr):
-			e = Tag(name = tag)
-			e.string = getattr(source, attr)
-			xml_elem.append(e)
+        if hasattr(source, attr):
+            e = Tag(name = tag)
+            e.string = getattr(source, attr)
+            xml_elem.append(e)
 
 def get_cmdline_arguments():
-	argparser = argparse.ArgumentParser()
-	argparser.add_argument('url', help = 'URL of the source RSS file')
-	argparser.add_argument('output', help = 'Path of the resulting RSS file. Use "-" for stdout')
-	argparser.add_argument('selector', nargs = '+', help = 'CSS selector used to extract the relevant content from the page linked by each RSS item. If more than one is provided, their results are concatened')
-	argparser.add_argument('-p', '--pretty', action = 'store_true', help = 'Specify that the output should be prettyfied')
-	argparser.add_argument('-r', '--replace-url', nargs = 2, help = 'Pattern and substitution used to replace URLs in img and a elements')
-	argparser.add_argument('--raw', action = 'store_true')
-	return argparser.parse_args()
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('url', help = 'URL of the source RSS file')
+    argparser.add_argument('output', help = 'Path of the resulting RSS file. Use "-" for stdout')
+    argparser.add_argument('selector', nargs = '+', help = 'CSS selector used to extract the relevant content from the page linked by each RSS item. If more than one is provided, their results are concatened')
+    argparser.add_argument('-p', '--pretty', action = 'store_true', help = 'Specify that the output should be prettyfied')
+    argparser.add_argument('-r', '--replace-url', nargs = 2, help = 'Pattern and substitution used to replace URLs in img and a elements')
+    argparser.add_argument('--raw', action = 'store_true')
+    return argparser.parse_args()
 
 def replace_urls(tags, regexp, repl):
-	for tag in tags:
-		if tag.name == 'a':
-			tag['href'] = regexp.sub(repl, tag['href'])
-		elif tag.name == 'img':
-			tag['src'] = regexp.sub(repl, tag['src'])
+    for tag in tags:
+        if tag.name == 'a':
+            tag['href'] = regexp.sub(repl, tag['href'])
+        elif tag.name == 'img':
+            tag['src'] = regexp.sub(repl, tag['src'])
 
-		for a in tag.find_all('a'):
-			a['href'] = regexp.sub(repl, a['href'])
-		for img in tag.find_all('img'):
-			img['src'] = regexp.sub(repl, img['src'])
+        for a in tag.find_all('a'):
+            a['href'] = regexp.sub(repl, a['href'])
+        for img in tag.find_all('img'):
+            img['src'] = regexp.sub(repl, img['src'])
 
-	return tags
+    return tags
 
 def rebuild_rss(url, output, selectors, replace = None, pretty = False, raw = False):
-	source = feedparser.parse(url)
+    source = feedparser.parse(url)
 
-	try:
-		soup = BeautifulSoup('<rss version="2.0" />', 'xml')
-		rss = soup.rss
-		has_lxml = True
-	except FeatureNotFound:
-		rss = BeautifulSoup('<rss version="2.0" />').rss
-		has_lxml = False
+    try:
+        soup = BeautifulSoup('<rss version="2.0" />', 'xml')
+        rss = soup.rss
+        has_lxml = True
+    except FeatureNotFound:
+        rss = BeautifulSoup('<rss version="2.0" />').rss
+        has_lxml = False
 
-	channel = Tag(name = 'channel')
-	rss.append(channel)
-	putback_elems(source.feed, channel_required, channel)
-	putback_elems(source.feed, channel_optional, channel)
+    channel = Tag(name = 'channel')
+    rss.append(channel)
+    putback_elems(source.feed, channel_required, channel)
+    putback_elems(source.feed, channel_optional, channel)
 
-	build_date = Tag(name = 'lastBuildDate')
-	build_date.string = time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime())
-	channel.append(build_date)
+    build_date = Tag(name = 'lastBuildDate')
+    build_date.string = time.strftime('%a, %d %b %Y %H:%M:%S +0000', time.gmtime())
+    channel.append(build_date)
 
-	generator = Tag(name = 'generator')
-	generator.string = source.feed.generator + ' & RSS Rebuilder' if hasattr(source.feed, 'generator') else 'RSS Rebuilder'
-	channel.append(generator)
+    generator = Tag(name = 'generator')
+    generator.string = source.feed.generator + ' & RSS Rebuilder' if hasattr(source.feed, 'generator') else 'RSS Rebuilder'
+    channel.append(generator)
 
-	if replace:
-		regexp = re.compile(replace[0])
+    if replace:
+        regexp = re.compile(replace[0])
 
-	for entry in source.entries:
-		item = Tag(name = 'item')
-		channel.append(item)
+    for entry in source.entries:
+        item = Tag(name = 'item')
+        channel.append(item)
 
-		putback_elems(entry, item_required, item)
-		putback_elems(entry, item_optional, item)
+        putback_elems(entry, item_required, item)
+        putback_elems(entry, item_optional, item)
 
-		r = requests.get(entry.link)
-		html = r.content if raw else r.text
-		linked_html = BeautifulSoup(html, 'lxml') if has_lxml else BeautifulSoup(html)
+        r = requests.get(entry.link)
+        html = r.content if raw else r.text
+        linked_html = BeautifulSoup(html, 'lxml') if has_lxml else BeautifulSoup(html)
 
-		content = ''
-		for selector in selectors:
-			tags = linked_html.select(selector)
-			if replace:
-				tags = replace_urls(tags, regexp, replace[1])
+        content = ''
+        for selector in selectors:
+            tags = linked_html.select(selector)
+            if replace:
+                tags = replace_urls(tags, regexp, replace[1])
 
-			content = reduce(lambda s, tag: s + unicode(tag), tags, content)
+            content = reduce(lambda s, tag: s + unicode(tag), tags, content)
 
-		desc = Tag(name = 'description')
-		desc.string = content
-		item.append(desc)
+        desc = Tag(name = 'description')
+        desc.string = content
+        item.append(desc)
 
-	out_func = lambda x: (x.prettify() if pretty else unicode(x)).encode('utf-8')
-	if output == '-':
-		out_file = sys.stdout
-		close_file = lambda: None
-	else:
-		out_file = open(output, 'w')
-		close_file = out_file.close
+    out_func = lambda x: (x.prettify() if pretty else unicode(x)).encode('utf-8')
+    if output == '-':
+        out_file = sys.stdout
+        close_file = lambda: None
+    else:
+        out_file = open(output, 'w')
+        close_file = out_file.close
 
-	if has_lxml:
-		out_file.write(out_func(soup))
-	else:
-		out_file.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
-		out_file.write(out_func(rss))
-	out_file.write('\n')
-	close_file()
+    if has_lxml:
+        out_file.write(out_func(soup))
+    else:
+        out_file.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
+        out_file.write(out_func(rss))
+    out_file.write('\n')
+    close_file()
 
 if __name__ == '__main__':
-	args = get_cmdline_arguments()
-	rebuild_rss(args.url, args.output, args.selector, args.replace_url, args.pretty, args.raw)
+    args = get_cmdline_arguments()
+    rebuild_rss(args.url, args.output, args.selector, args.replace_url, args.pretty, args.raw)
 
